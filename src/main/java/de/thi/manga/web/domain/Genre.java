aa155ac3d403f378c4c75951f2f7b1da18ba80f5
Daniel <thesorekz@gmail.com>
2015-10-24 20:21:24 +0200

    Add startpage and first draft of the domain objects

---
+++ src/main/java/de/thi/manga/web/domain/Genre.java
@@ -0,0 +1,17 @@
+package de.thi.manga.web.domain;
+
+/**
+ * Created by daniel on 24.10.15.
+ */
+public enum Genre {
+    Adventure, Action,
+    Cyberpunk,
+    Ecchi,
+    Horror,
+    Magical_Girl, Mecha, Mystery,
+    Slice_Of_Life, Sport,
+
+    // Target Groups:
+    Shounen, Seinen,
+    Shoujo, Josei,
+}
