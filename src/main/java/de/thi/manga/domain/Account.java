bf80f170eb633c356088f4721130be86d475ed21
Daniel <thesorekz@gmail.com>
2016-01-19 19:57:02 +0100

    Account name in der Datenbank unique machen

--- src/main/java/de/thi/manga/domain/Account.java
+++ src/main/java/de/thi/manga/domain/Account.java
@@ -1,9 +1,6 @@
 package de.thi.manga.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -13,6 +10,7 @@ public class Account implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     private String displayName;
